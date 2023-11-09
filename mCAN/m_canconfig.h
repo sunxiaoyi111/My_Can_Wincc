18361fd518e6f0ed0c9510204908716cb2431788
sun.xiaoyi <571306815@qq.com>
2023-11-09 23:42:43 +0800

    Title:增加配置项类 Detail: 增加配置项类 准备开始写必要的存储配置项 Self-test: PASS Commit dependency: NO

---
+++ mCAN/m_canconfig.h
@@ -0,0 +1,19 @@
+#ifndef M_CANCONFIG_H
+#define M_CANCONFIG_H
+
+#include <QObject>
+#include <QSettings> // 引入QSettings头文件
+#include <QCoreApplication> // 引入QCoreApplication头文件
+#include <QDir> // 引入QDir头文件
+class m_canconfig
+{
+public:
+    m_canconfig();
+    ~m_canconfig();
+
+    void m_canconfig_init();
+private:
+    QString mcfg_bb;
+};
+
+#endif // M_CANCONFIG_H
