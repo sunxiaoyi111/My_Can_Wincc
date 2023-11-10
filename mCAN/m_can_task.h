899a21f4d18d8c81f7e0a97ab8a22acfd81a3045
sun.xiaoyi <571306815@qq.com>
2023-11-10 23:32:09 +0800

    Title:D3优化 Detail: 1.增加设备打开、关闭UI按钮 2.增加CAN报文基础帧格式 3、初步增加接受TaskO Self-test: PASS Commit dependency: NO

--- mCAN/m_can_task.h
+++ mCAN/m_can_task.h
@@ -1,9 +1,10 @@
 #ifndef M_CAN_TASK_H
 #define M_CAN_TASK_H
+#include <QObject>
 
 #include <QDebug>
 #include <QThread>
-
+#include <QMessageBox>
 
 #include "canlib/zlgcan/zlgcan_x64/zlgcan.h"
 #include "canlib/zlgcan/zlgcan_x64/canframe.h"
@@ -12,12 +13,28 @@
 
 
 
-class M_Can_Task
+class M_Can_Task : public QObject
 {
 public:
     M_Can_Task();
     ~M_Can_Task();
     int open_device_test();
+    int open_device();
+    int mct_close_device();
+    void mct_msgBox_Show(QString msg);
+
+    bool isStart;
+
+    DEVICE_HANDLE mct_dhandle;
+    CHANNEL_HANDLE mct_chHandle;
+    ZCAN_Transmit_Data mct_frame;
+    ZCAN_CHANNEL_INIT_CONFIG mct_cfg;
+    BYTE test_data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+
+
+    void mct_DataRecive();
+
 };
 
 #endif // M_CAN_TASK_H
--- mCAN/m_canwidget.cpp
+++ mCAN/m_canwidget.cpp
@@ -10,15 +10,45 @@ M_CANWidget::M_CANWidget(QWidget *parent)
     , ui(new Ui::M_CANWidget)
 {
     ui->setupUi(this);
+    //链接默认的按钮和信号与槽
+    connect(this,&M_CANWidget::sig_in_num,this,&M_CANWidget::ret_any_in);
     //CAN收发任务初始化
-    can = new M_Can_Task;
+    m_can = new M_Can_Task;
+
+
+    //新建一个线程对象
+    my_Can_Thread = new QThread(this);
+    //使用movetothread 方法，m_can_task类中的方法将在新的线程中执行
+    m_can->moveToThread(my_Can_Thread);
+
+
+
+
+
     connect(ui->pushButton,&QPushButton::clicked,this,[=](){
         qDebug()<< "You clicked the button!";
-        can->open_device_test();
+//        can->open_device_test();
     });
-
-    //链接默认的按钮和信号与槽
-
+    connect(ui->toolButton_DevConnect,&QPushButton::clicked,this,[=](){
+        qDebug()<<"connect";
+        int ret ;
+        if(ui->toolButton_DevConnect->text()=="连接并启动")
+        {
+            ret = m_can->open_device();
+            if(ret == 0)
+            {
+                ui->toolButton_DevConnect->setText("断开连接");
+            }
+        }
+        else
+        {
+            m_can->mct_close_device();
+            ui->toolButton_DevConnect->setText("连接并启动");
+        }
+//        int ret = 0 ;
+//        ret = sig_in_num(10);
+//        qDebug()<<ret;
+    });
 
 
 
@@ -28,6 +58,10 @@ M_CANWidget::M_CANWidget(QWidget *parent)
 M_CANWidget::~M_CANWidget()
 {
     delete ui;
-    delete(can);
+    delete(m_can);
 }
 
+int M_CANWidget::ret_any_in(int in)
+{
+    return in;
+}
