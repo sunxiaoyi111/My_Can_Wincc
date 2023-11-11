4657d9a403ae33d33b3899f854e476b77b990774
sun.xiaoyi <571306815@qq.com>
2023-11-11 10:26:29 +0800

    Title: d4提交

--- mCAN/m_canwidget.cpp
+++ mCAN/m_canwidget.cpp
@@ -51,6 +51,7 @@ M_CANWidget::M_CANWidget(QWidget *parent)
         {
             m_can->mct_close_device();
             ui->toolButton_DevConnect->setText("连接并启动");
+            m_can->isStart = false;
             if(my_Can_Thread->isRunning())
             {
 //                 CanSet->CanClose();
